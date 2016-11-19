0792302e9852182d2a1bac08265bca886aa42a53
Arror <maqiang_ios@163.com>
2016-11-19 21:50:04 +0800

    Defined and implement hallo server

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "common/protos"
 )
 
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) SayHallo(ctx context.Context, in *pb.HalloRequest) (*pb.HalloReply, error) {
+	return &pb.HalloReply{Message: "Hello " + in.Name}, nil
+}
+
 func main() {
-	fmt.Println("Hallo grpc.")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to server: %v", err)
+	}
 }
